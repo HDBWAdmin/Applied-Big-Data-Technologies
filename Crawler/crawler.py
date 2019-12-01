850bf87bd038e9beeae88d837348cde264a69468
root <root@projecthdbw.lu3v2makg2peteotrpkdeddhod.ax.internal.cloudapp.net>
2019-12-01 15:25:01 +0000

    Daily Github Repository Update at  Sun Dec  1 15:25:01 UTC 2019

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #Crawler for Project Applied-Big-Data-Technologies
 #with daily Repository Upload at 20:15
-#log information Updated +4
+#log information Updated +5
 
 
 import feedparser
